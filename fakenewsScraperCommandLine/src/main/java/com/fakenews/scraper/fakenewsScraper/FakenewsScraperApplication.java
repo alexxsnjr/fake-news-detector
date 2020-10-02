b8c7448b3aa69e9002179e73d0b4fe7f1e814623
Alexxsnjr <alexxsnjr@gmail.com>
2020-10-02 18:59:47 +0200

    Remove Database dependencies from scraperComandLine

--- fakenewsScraperCommandLine/src/main/java/com/fakenews/scraper/fakenewsScraper/FakenewsScraperApplication.java
+++ fakenewsScraperCommandLine/src/main/java/com/fakenews/scraper/fakenewsScraper/FakenewsScraperApplication.java
@@ -1,8 +1,10 @@
 package com.fakenews.scraper.fakenewsScraper;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,8 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableFeignClients
 @RibbonClient(name="news-api")
 @SpringBootApplication
-@EnableSpringConfigured
-@EnableJpaRepositories("com.fakenews.commons.fakenewsCommons.models.dao")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EntityScan({"com.fakenews.commons.fakenewsCommons.models"})
 public class FakenewsScraperApplication {
 
